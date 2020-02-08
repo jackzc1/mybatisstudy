81741ced3d01aeb63305d01d0fceea10b2ba8be1
zc <zc@qq.com>
2020-02-08 15:57:57 +0800

    入门程序完成

---
+++ src/test/java/com/soft/test/Demo1.java
@@ -0,0 +1,35 @@
+package com.soft.test;
+
+import com.soft.dao.UserDao;
+import com.soft.entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2020/2/8.
+ */
+public class Demo1 {
+    @Test
+    public void testFindAll() throws Exception {
+
+        InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> list = userDao.findAll();
+        for (User user : list) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+        inputStream.close();
+
+    }
+
+}
